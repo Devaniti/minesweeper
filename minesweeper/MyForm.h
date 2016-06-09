87c597d18dbf9fe067c4aa6839cc87cf568e53fd
Dima Bulatov <Dima Bulatov>
2016-06-09 23:49:35 +0300

    Implented custom field\small improvments

--- minesweeper/MyForm.h
+++ minesweeper/MyForm.h
@@ -41,6 +41,13 @@ namespace minesweeper {
 	private: System::Windows::Forms::Button^  button2;
 	private: System::Windows::Forms::Button^  button3;
 	private: System::Windows::Forms::Button^  button4;
+	private: System::Windows::Forms::Button^  button5;
+	private: System::Windows::Forms::NumericUpDown^  numericUpDown1;
+	private: System::Windows::Forms::NumericUpDown^  numericUpDown2;
+	private: System::Windows::Forms::NumericUpDown^  numericUpDown3;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::Label^  label3;
 
 
 	protected:
@@ -64,7 +71,17 @@ namespace minesweeper {
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->button3 = (gcnew System::Windows::Forms::Button());
 			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->button5 = (gcnew System::Windows::Forms::Button());
+			this->numericUpDown1 = (gcnew System::Windows::Forms::NumericUpDown());
+			this->numericUpDown2 = (gcnew System::Windows::Forms::NumericUpDown());
+			this->numericUpDown3 = (gcnew System::Windows::Forms::NumericUpDown());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown2))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown3))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// pictureBox1
@@ -79,7 +96,7 @@ namespace minesweeper {
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(114, 79);
+			this->button1->Location = System::Drawing::Point(113, 47);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(75, 23);
 			this->button1->TabIndex = 1;
@@ -89,7 +106,7 @@ namespace minesweeper {
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(114, 129);
+			this->button2->Location = System::Drawing::Point(113, 96);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(75, 23);
 			this->button2->TabIndex = 2;
@@ -99,7 +116,7 @@ namespace minesweeper {
 			// 
 			// button3
 			// 
-			this->button3->Location = System::Drawing::Point(114, 179);
+			this->button3->Location = System::Drawing::Point(113, 145);
 			this->button3->Name = L"button3";
 			this->button3->Size = System::Drawing::Size(75, 23);
 			this->button3->TabIndex = 3;
@@ -109,7 +126,7 @@ namespace minesweeper {
 			// 
 			// button4
 			// 
-			this->button4->Location = System::Drawing::Point(143, 224);
+			this->button4->Location = System::Drawing::Point(143, 17);
 			this->button4->Name = L"button4";
 			this->button4->Size = System::Drawing::Size(75, 23);
 			this->button4->TabIndex = 4;
@@ -118,11 +135,87 @@ namespace minesweeper {
 			this->button4->Visible = false;
 			this->button4->Click += gcnew System::EventHandler(this, &MyForm::button4_Click);
 			// 
+			// button5
+			// 
+			this->button5->Location = System::Drawing::Point(113, 194);
+			this->button5->Name = L"button5";
+			this->button5->Size = System::Drawing::Size(75, 23);
+			this->button5->TabIndex = 5;
+			this->button5->Text = L"Custom";
+			this->button5->UseVisualStyleBackColor = true;
+			this->button5->Click += gcnew System::EventHandler(this, &MyForm::button5_Click);
+			// 
+			// numericUpDown1
+			// 
+			this->numericUpDown1->Location = System::Drawing::Point(12, 237);
+			this->numericUpDown1->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 70, 0, 0, 0 });
+			this->numericUpDown1->Minimum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 8, 0, 0, 0 });
+			this->numericUpDown1->Name = L"numericUpDown1";
+			this->numericUpDown1->Size = System::Drawing::Size(72, 20);
+			this->numericUpDown1->TabIndex = 6;
+			this->numericUpDown1->Value = System::Decimal(gcnew cli::array< System::Int32 >(4) { 8, 0, 0, 0 });
+			this->numericUpDown1->ValueChanged += gcnew System::EventHandler(this, &MyForm::numericUpDown1_ValueChanged);
+			// 
+			// numericUpDown2
+			// 
+			this->numericUpDown2->Location = System::Drawing::Point(114, 237);
+			this->numericUpDown2->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 40, 0, 0, 0 });
+			this->numericUpDown2->Minimum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 8, 0, 0, 0 });
+			this->numericUpDown2->Name = L"numericUpDown2";
+			this->numericUpDown2->Size = System::Drawing::Size(72, 20);
+			this->numericUpDown2->TabIndex = 7;
+			this->numericUpDown2->Value = System::Decimal(gcnew cli::array< System::Int32 >(4) { 8, 0, 0, 0 });
+			this->numericUpDown2->ValueChanged += gcnew System::EventHandler(this, &MyForm::numericUpDown2_ValueChanged);
+			// 
+			// numericUpDown3
+			// 
+			this->numericUpDown3->Location = System::Drawing::Point(216, 237);
+			this->numericUpDown3->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 54, 0, 0, 0 });
+			this->numericUpDown3->Minimum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 10, 0, 0, 0 });
+			this->numericUpDown3->Name = L"numericUpDown3";
+			this->numericUpDown3->Size = System::Drawing::Size(72, 20);
+			this->numericUpDown3->TabIndex = 8;
+			this->numericUpDown3->Value = System::Decimal(gcnew cli::array< System::Int32 >(4) { 10, 0, 0, 0 });
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(28, 260);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(35, 13);
+			this->label1->TabIndex = 9;
+			this->label1->Text = L"Width";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(128, 260);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(38, 13);
+			this->label2->TabIndex = 10;
+			this->label2->Text = L"Height";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(231, 260);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(35, 13);
+			this->label3->TabIndex = 11;
+			this->label3->Text = L"Mines";
+			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(301, 300);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->numericUpDown3);
+			this->Controls->Add(this->numericUpDown2);
+			this->Controls->Add(this->numericUpDown1);
+			this->Controls->Add(this->button5);
 			this->Controls->Add(this->button4);
 			this->Controls->Add(this->pictureBox1);
 			this->Controls->Add(this->button3);
@@ -135,34 +228,66 @@ namespace minesweeper {
 			this->Text = L"Minesweeper";
 			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown2))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown3))->EndInit();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
 	private: System::Void MyForm_Load(System::Object^  sender, System::EventArgs^  e) {
 	}
-
-	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
-		GameField = TGameField(9, 9, 10);
+	void HideButtons()
+	{
 		button1->Visible = false;
 		button2->Visible = false;
 		button3->Visible = false;
+		button5->Visible = false;
+		numericUpDown1->Visible = false;
+		numericUpDown2->Visible = false;
+		numericUpDown3->Visible = false;
+		label1->Visible = false;
+		label2->Visible = false;
+		label3->Visible = false;
+	}	
+	void ShowButtons()
+	{
+		button1->Visible = true;
+		button2->Visible = true;
+		button3->Visible = true;
+		button5->Visible = true;
+		numericUpDown1->Visible = true;
+		numericUpDown2->Visible = true;
+		numericUpDown3->Visible = true;
+		label1->Visible = true;
+		label2->Visible = true;
+		label3->Visible = true;
+	}
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+		GameField = TGameField(9, 9, 10);
+		HideButtons();
 		GameField.InitGraphics(pictureBox1, this);
 	}
 	private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e)
 	{
 		GameField = TGameField(16, 16, 40);
-		button1->Visible = false;
-		button2->Visible = false;
-		button3->Visible = false;
+		HideButtons();
 		GameField.InitGraphics(pictureBox1, this);
 	}
 	private: System::Void button3_Click(System::Object^  sender, System::EventArgs^  e)
 	{
 		GameField = TGameField(24, 20, 99);
-		button1->Visible = false;
-		button2->Visible = false;
-		button3->Visible = false;
+		HideButtons();
+		GameField.InitGraphics(pictureBox1, this);
+	}
+	private: System::Void button5_Click(System::Object^  sender, System::EventArgs^  e) {
+		int w, h, mines;
+		w = Convert::ToSingle(numericUpDown1->Value);
+		h = Convert::ToSingle(numericUpDown2->Value);
+		mines = Convert::ToSingle(numericUpDown3->Value);
+		GameField = TGameField(w, h, mines);
+		HideButtons();
 		GameField.InitGraphics(pictureBox1, this);
 	}
 
@@ -184,9 +309,7 @@ private: System::Void button4_Click(System::Object^  sender, System::EventArgs^ 
 	this->Height = 339;
 	pictureBox1->Width = 1;
 	pictureBox1->Height = 1;
-	button1->Visible = true;
-	button2->Visible = true;
-	button3->Visible = true;
+	ShowButtons();
 	button4->Visible = false;
 }
 private: System::Void pictureBox1_MouseDoubleClick(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e) {
@@ -198,6 +321,13 @@ private: System::Void pictureBox1_MouseDoubleClick(System::Object^  sender, Syst
 	y /= 25;
 	if (e->Button == System::Windows::Forms::MouseButtons::Left)
 		GameField.SmartClick(x, y, pictureBox1, button4);
+}
+
+private: System::Void numericUpDown1_ValueChanged(System::Object^  sender, System::EventArgs^  e) {
+	numericUpDown3->Maximum = (int)(Convert::ToSingle(numericUpDown1->Value)*Convert::ToSingle(numericUpDown2->Value))-10;
+}
+private: System::Void numericUpDown2_ValueChanged(System::Object^  sender, System::EventArgs^  e) {
+	numericUpDown3->Maximum = (int)(Convert::ToSingle(numericUpDown1->Value)*Convert::ToSingle(numericUpDown2->Value))-10;
 }
 };
 }
